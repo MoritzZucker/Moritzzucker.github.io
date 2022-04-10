7c1fa4862dbf7146ba535324d2ecec6e140657e3
MoritzZucker <62714043+MoritzZucker@users.noreply.github.com>
2022-04-10 11:47:31 +0200

    second commit

--- index.js
+++ index.js
@@ -1,77 +1,106 @@
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser')
-const db = new sqlite3.Database('./db/comment.db');
 
-const port = process.env.PORT || 3000;
-const app = express();
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use( express.static( "public" ) );
-
-app.get('/', async (req, res) => {
-  db.all('SELECT * FROM comments', (err, comments) => {
-    res.render('pages/index', { comments })
-  });
-});
-
-app.get('/comment', async (req, res) => {
-  db.all('SELECT * FROM comments', (err, comments) => {
-    res.render('pages/comment', { comments })
-  });
-});
-
-app.get('/', (req, res) => {
-  res.render('pages/', { success: true });
-});
-
-app.get('/gallery', (req, res) => {
-  res.render('pages/gallery', { success: true });
-});
-
-app.get('/anmelden', (req, res) => {
-  res.render('pages/anmelden', { success: true });
-});
-
-app.get('/partner', (req, res) => {
-  res.render('pages/partner', { success: true });
-});
-
-app.get('/spenden', (req, res) => {
-  res.render('pages/spenden', { success: true });
-});
-
-app.get('/unternehmen', (req, res) => {
-  res.render('pages/unternehmen', { success: true });
-});
-
-app.get('/zieleunderfolge', (req, res) => {
-  res.render('pages/zieleunderfolge', { success: true });
-});
-
-app.post('/comment', (req, res) => {
-  if (req.body.Vorname && req.body.Nachname && req.body.Kommentar) {
-    db.run('INSERT INTO comments(Vorname, Nachname, Kommentar) VALUES (?, ?, ?);', [req.body.Vorname, req.body.Nachname, req.body.Kommentar], function (err) {
-      if(err) {
-        res.json({error: err});
-      } else {
-        res.json({
-         ...req.body, 
-         id: this.lastID,
-      });
+(function () {
+    "use strict";
+  
+    const items = [
+      "🍭",
+      "❌",
+      "⛄️",
+      "🦄",
+      "🍌",
+      "💩",
+      "👻",
+      "😻",
+      "💵",
+      "🤡",
+      "🦖",
+      "🍎"
+    ];
+    document.querySelector(".info").textContent = items.join(" ");
+  
+    const doors = document.querySelectorAll(".door");
+    document.querySelector("#spinner").addEventListener("click", spin);
+    document.querySelector("#reseter").addEventListener("click", init);
+  
+    async function spin() {
+      init(false, 1, 2);
+      for (const door of doors) {
+        const boxes = door.querySelector(".boxes");
+        const duration = parseInt(boxes.style.transitionDuration);
+        boxes.style.transform = "translateY(0)";
+        await new Promise((resolve) => setTimeout(resolve, duration * 100));
       }
-    });
-  } else {
-    res.json({error: "Request body is not correct"});
     }
-});
-
-const server = app.listen(port, () => {
- console.log(`Server listening on port ${port}…`)
-});
-
-module.exports = server
+  
+    function init(firstInit = true, groups = 1, duration = 1) {
+      for (const door of doors) {
+        if (firstInit) {
+          door.dataset.spinned = "0";
+        } else if (door.dataset.spinned === "1") {
+          return;
+        }
+  
+        const boxes = door.querySelector(".boxes");
+        const boxesClone = boxes.cloneNode(false);
+  
+        const pool = ["❓"];
+        if (!firstInit) {
+          const arr = [];
+          for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
+            arr.push(...items);
+          }
+          pool.push(...shuffle(arr));
+  
+          boxesClone.addEventListener(
+            "transitionstart",
+            function () {
+              door.dataset.spinned = "1";
+              this.querySelectorAll(".box").forEach((box) => {
+                box.style.filter = "blur(1px)";
+              });
+            },
+            { once: true }
+          );
+  
+          boxesClone.addEventListener(
+            "transitionend",
+            function () {
+              this.querySelectorAll(".box").forEach((box, index) => {
+                box.style.filter = "blur(0)";
+                if (index > 0) this.removeChild(box);
+              });
+            },
+            { once: true }
+          );
+        }
+        // console.log(pool);
+  
+        for (let i = pool.length - 1; i >= 0; i--) {
+          const box = document.createElement("div");
+          box.classList.add("box");
+          box.style.width = door.clientWidth + "px";
+          box.style.height = door.clientHeight + "px";
+          box.textContent = pool[i];
+          boxesClone.appendChild(box);
+        }
+        boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
+        boxesClone.style.transform = `translateY(-${
+          door.clientHeight * (pool.length - 1)
+        }px)`;
+        door.replaceChild(boxesClone, boxes);
+        // console.log(door);
+      }
+    }
+  
+    function shuffle([...arr]) {
+      let m = arr.length;
+      while (m) {
+        const i = Math.floor(Math.random() * m--);
+        [arr[m], arr[i]] = [arr[i], arr[m]];
+      }
+      return arr;
+    }
+  
+    init();
+  })();
+  
